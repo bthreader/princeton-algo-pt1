05a4fefb1a284e053d8e1a470ec845005e3a6da5
bthreader <threaderb@hotmail.co.uk>
2023-01-31 22:51:21 +0000

    make skeleton

--- src/main/java/princeton/w1/PercolationStats.java
+++ src/main/java/princeton/w1/PercolationStats.java
@@ -1,11 +1,20 @@
 package princeton.w1;
 
+import edu.princeton.cs.algs4.StdRandom;
+
 public class PercolationStats {
+    private double[] percolationThresholds;
 
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
-        if (n >= 0 || trials <= 0) {
+        if (trials <= 0) {
             throw new IllegalArgumentException();
+        }
+
+        this.percolationThresholds = new double[n];
+
+        for (int i = 0; i < trials; i++) {
+            performTrial(i, n);
         }
     }
 
@@ -35,5 +44,21 @@ public class PercolationStats {
         double x;
         x = 100.00;
         return x;
+    }
+
+    private void performTrial(int trialIndex, int n) {
+        Percolation percolation = new Percolation(n);
+
+        int x;
+        int y;
+        boolean[] blockedSites;
+        while (!percolation.percolates()) {
+            // Change this to only sample blocked sites
+            x = StdRandom.uniformInt(n-1)+1;
+            y = StdRandom.uniformInt(n-1)+1;
+            percolation.open(x, y);
+        }
+
+        percolationThresholds[trialIndex] = 2/3;
     }
 }
